85fd59c032f0247204f628fdbef429f0effbd265
jonatell <jonathanbtellez@gmail.com>
2024-10-01 21:59:43 -0500

    handle port

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/v1', app, document);
   
-  await app.listen(envs.port);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
   console.log(`Server running on port ${envs.port}`)
 }
 bootstrap();
